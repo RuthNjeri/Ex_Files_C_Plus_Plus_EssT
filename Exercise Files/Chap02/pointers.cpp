3b265c9f096dfb2cdcfe16365cb7db71e16feb08
Ruth Waiganjo <ruthwaiganjo@microsoft.com>
2021-12-07 09:58:22 +0300

    pointers

--- Exercise Files/Chap02/pointers.cpp
+++ Exercise Files/Chap02/pointers.cpp
@@ -5,12 +5,28 @@
 int main()
 {
     int x = 7;
-    int y = 42;
-    int * ip = &x;
+    int * ip = & x; // ip points to the address of x
+    int & y = x; // Y is a reference, to x
 
+    int* c = & y;
+
+    printf("The value of c is %d\n", c);
+    printf("The value of ip is %d\n", ip);
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %d\n", y); // No need to dereference
+    printf("The value of *ip is %d\n", *ip); // Dereferencing, getting the value at the address
+
+    y = 73; // There is no syntax to change a reference, here, X is being changed not Y
     printf("The value of x is %d\n", x);
     printf("The value of y is %d\n", y);
-    printf("The value of *ip is %d\n", *ip);
+    printf("The value of *ip is %d\n", *ip); // Dereferencing, getting the value at the address
+
+    int z = 7;
+    ip = &z;
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %d\n", y);
+    printf("The value of *ip is %d\n", *ip); // Dereferencing, getting the value at the address
+
 
     return 0;
 }

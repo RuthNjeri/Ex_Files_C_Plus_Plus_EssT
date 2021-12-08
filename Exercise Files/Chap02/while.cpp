e94cb43c57e1ad2bc898f19893e7e68c83add613
Ruth Waiganjo <ruthwaiganjo@microsoft.com>
2021-12-08 09:36:34 +0300

    while and do while

--- Exercise Files/Chap02/while.cpp
+++ Exercise Files/Chap02/while.cpp
@@ -7,10 +7,20 @@ int main()
     int array[] = { 1, 2, 3, 4, 5 };
     int i = 0;
     
-    while(i < 5) {
+    while(i < 5) { // Loop control, controls the execution of the loop
+        if (i == 2) {
+            ++i;
+            continue; // skips the rest of the loop but still continues.
+        }
+        //if (i == 3) break; // You can break out of a loop prematurely. You do not need curly braces if it is only one statement in a block.
         printf("element %d is %d\n", i, array[i]);
         ++i;
     }
+
+    //do { 
+    //    printf("element %d is %d\n", i, array[i]);
+    //    ++i;
+    //} while (i < 5);// The test is at the bottom
     
     return 0;
 }

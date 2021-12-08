df1ca0b8f1ede63296f19985b279d28fac3f84d5
Ruth Waiganjo <ruthwaiganjo@microsoft.com>
2021-12-08 09:09:09 +0300

    primitive strings

--- Exercise Files/Chap02/working.cpp
+++ Exercise Files/Chap02/working.cpp
@@ -3,7 +3,19 @@
 #include <cstdio>
 
 int main()
-{
-    puts("Hello, World!");
+{   // Primitive string is a collection of strings terminated by a 0
+    char s[] = "String"; // same as char s[] = {'S', 't', 'r', 'i', 'n', 'g', 0};
+    printf("s is %s\n", s);
+
+    for(int i = 0; s[i] != 0; ++i) {
+        printf("char is %c\n", s[i]);
+    }
+
+    // The above can also be done by pointers
+    for (char* cp = s; *cp != 0; ++cp) {
+        printf("char is %c\n", *cp);
+    }
+
+
     return 0;
 }

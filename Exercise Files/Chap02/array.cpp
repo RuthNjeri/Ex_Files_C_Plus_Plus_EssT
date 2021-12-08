f238b01f80576dc60f6d8028e3a04b9273a16dca
Ruth Waiganjo <ruthwaiganjo@microsoft.com>
2021-12-08 08:52:43 +0300

    primitive array

--- Exercise Files/Chap02/array.cpp
+++ Exercise Files/Chap02/array.cpp
@@ -9,6 +9,20 @@ int main()
 
     printf("x is %d\n", x);
 
+    // Array elements may be accessed as if the array was a pointer
+    // Pointers are strongly typed and know the size of the type you are pointing to
+    *array = 6;
+    
+    printf("%d", array[0]);
+
+    // Assigns the address of the array to the pointer. You do not need the address operator because the array may be accessed as if it were a pointer
+
+    int* ip = array;
+    *(++ip) = 7;
+
+    //*ip = 2;
+
+
     array[2] = 47;
 
     for(int i : array) {

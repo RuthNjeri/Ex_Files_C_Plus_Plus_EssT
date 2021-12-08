b1cc4c4ce68f636b6ea11e128f66f5714cf0a766
Ruth Waiganjo <ruthwaiganjo@microsoft.com>
2021-12-08 09:51:29 +0300

    range based for loop

--- Exercise Files/Chap02/range-for.cpp
+++ Exercise Files/Chap02/range-for.cpp
@@ -6,8 +6,16 @@ int main()
 {
     int array[] = { 1, 2, 3, 4, 5 };
 
-    for(int i : array) {
+    for(int i : array) { // array is a container, for each element of the container, it is set to the variable i
         printf("i is %d\n", i);
     }
+
+    // With a string
+    char s[] = "String";
+    for (char c : s) {
+        if (c == 0) break;
+        printf("c is %c\n", c);
+    }
+
     return 0;
 }

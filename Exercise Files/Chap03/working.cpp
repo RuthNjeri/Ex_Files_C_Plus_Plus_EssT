5b670a760e377cf38678df549db3d2bbb2d6f2fd
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-12-22 11:27:26 +0300

    references

--- Exercise Files/Chap03/working.cpp
+++ Exercise Files/Chap03/working.cpp
@@ -2,8 +2,15 @@
 // updated 2020-06-24
 #include <cstdio>
 
+const int & f(const int & i){
+    return i;
+}
+
+
 int main()
 {
-    puts("Hello, World!");
+    int i = 5;
+    printf("f(i) is %d\n", i);
+    
     return 0;
 }

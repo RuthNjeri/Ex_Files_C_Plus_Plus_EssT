35a256fc5b73fecd8475a2c23de4329e9cffa795
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-12-22 13:12:09 +0300

    auto

--- Exercise Files/Chap03/auto-type2.cpp
+++ Exercise Files/Chap03/auto-type2.cpp
@@ -5,7 +5,7 @@
 
 int main() {
     std::vector<int> vi = { 1, 2, 3, 4, 5 };
-    for(std::vector<int>::iterator it = vi.begin(); it != vi.end(); ++it) {
+    for(auto it = vi.begin(); it != vi.end(); ++it) {
         printf("int is %d\n", *it);
     }
     return 0;

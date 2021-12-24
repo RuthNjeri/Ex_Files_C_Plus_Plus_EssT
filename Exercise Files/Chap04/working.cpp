c59c704631bce81aa40350e663c29d8453427de0
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-12-24 13:00:45 +0300

    compound operator

--- Exercise Files/Chap04/working.cpp
+++ Exercise Files/Chap04/working.cpp
@@ -7,6 +7,10 @@ int main()
     int x = 24; // Assignment operator = makes a copy of the right side to the left side
     int y = 43;
     
+    y += x; // Compound assignment operators, value of x add it to y  and leave the result in y without any copy.
+    
+    y = y + x; // Similar to the compound assignment but y is evaluated twice on the left and right, but on compound assignemnt y is evaluated once..compound assignment may be better in many occasions.
+    
     printf("X is %d\n", x);
     printf("Y is %d\n", y);
 }

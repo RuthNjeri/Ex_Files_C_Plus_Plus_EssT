4dbbbca4d11dbaafdb7466c419c6a5e8e0c3e2e8
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-12-27 15:55:15 +0300

    function pointer

--- Exercise Files/Chap05/func.cpp
+++ Exercise Files/Chap05/func.cpp
@@ -1,13 +1,11 @@
 // func.cpp by Bill Weinman <http://bw.org/>
 // updated 2020-06-24
 #include <cstdio>
-#include <string>
 
 
-const std::string & func()
+void func()
 {
-    const static std::string s = "this is from func"; // do not want to store in a stack, use static, always use static for anything being returned by reference, and const unless you want to change the value
-    return s;
+    puts("This is func()");
 }
 
 
@@ -15,7 +13,8 @@ const std::string & func()
 int main()
 {
     puts("this is main()");
-    const std::string s = func();
-    printf("s is %s\n", s.c_str()); // Returned by reference, by default and for small items it is returned by reference.
+    void(*pfunc)() = func; // Function pointer
+    (*pfunc)();
+    pfunc();// Ambiguous and not recommended, it is better to show that you are calling a function pointer
     return 0;
 }

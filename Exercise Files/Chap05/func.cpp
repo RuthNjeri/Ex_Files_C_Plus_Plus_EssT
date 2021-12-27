e92e8ef370914e42d05c5645b5d60f06307eb797
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-12-27 15:52:14 +0300

    fun return values

--- Exercise Files/Chap05/func.cpp
+++ Exercise Files/Chap05/func.cpp
@@ -1,30 +1,21 @@
 // func.cpp by Bill Weinman <http://bw.org/>
 // updated 2020-06-24
 #include <cstdio>
-#include "func.h"
+#include <string>
+
+
+const std::string & func()
+{
+    const static std::string s = "this is from func"; // do not want to store in a stack, use static, always use static for anything being returned by reference, and const unless you want to change the value
+    return s;
+}
 
 
 
 int main()
 {
     puts("this is main()");
-    func();
-    func();
-    func();
-    func();
-    func();
+    const std::string s = func();
+    printf("s is %s\n", s.c_str()); // Returned by reference, by default and for small items it is returned by reference.
     return 0;
 }
-
-void func()
-{
-    static int i = 5; // Default is automatic storage, which is stored in a stack and is temporary. Static stores the value in the main memory pool not the stack. The value is carried forward each time.
-    // Automatic storage is not recommended for larger objects.
-    printf("i is %d\n", i++);
-}
-
-// C++ by default passes by value when using parameters in a func
-// The values are usually stored in a stack.
-// Pass by reference using a reference operator(const qualified) or pointer for objects like strings
-// This is a preferred standard than storing it inside a stack
-

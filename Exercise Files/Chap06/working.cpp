a6cca574b34d08f45a1b2bc3de6c525098704bf1
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-12-28 15:24:32 +0300

    classes and struct

--- Exercise Files/Chap06/working.cpp
+++ Exercise Files/Chap06/working.cpp
@@ -2,8 +2,27 @@
 // updated 2020-06-24
 #include <cstdio>
 
+// Can create a class with a struct
+
+//struct A {// Members default to public
+//    int ia;
+//    int ib;
+//    int ic;
+//};
+
+class A {
+public:
+    int ia;
+    int ib;
+    int ic;
+};
+
+
+
 int main()
 {
-    puts("Hello, World!");
+    A a = {1, 2, 3};
+    
+    printf("ia is %d, ib is %d, ic is %d\n", a.ia, a.ib, a.ic);
     return 0;
 }

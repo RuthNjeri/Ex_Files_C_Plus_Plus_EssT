f5217288e926243cfc4d4174a24e13532ae59ae9
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-12-28 15:21:12 +0300

    classes and objects

--- Exercise Files/Chap06/class.cpp
+++ Exercise Files/Chap06/class.cpp
@@ -2,15 +2,26 @@
 // updated 2020-06-24
 #include <cstdio>
 
-// a very simple class
-class C1 {
-    int i = 0;
+// a very simple class, would often be in a header file
+class C1 {// Class definitaion/ interface
+    int i = 0; // Default to private..encapsulation
 public:
-    void setvalue( int value ) { i = value; }
-    int getvalue() { return i; }
+    void setvalue( int value );
+    int getvalue();
 };
 
-int main() {
+// Best practise to separate interface and definition, would be in a separate cpp file
+void C1::setvalue(int value){
+    i = value;
+}
+
+int C1::getvalue(){
+    return i;
+}
+
+
+
+int main() { // Would be in a separate cpp file
     int i = 47;
     C1 o1;
     
